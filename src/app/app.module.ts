8613db66a77f40ed48c5c232ecac7557921b0d79
manmohan <manmohan.sharma@edgenetworks.in>
2017-09-12 09:15:42 +0530

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CoursesService } from './courses.service';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './course.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+  CoursesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
