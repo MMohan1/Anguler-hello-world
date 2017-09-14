4622d439f8a85e1a417747131af8250fdb922d78
MMohan1 <manmohansharma987@gmail.com>
2017-09-14 10:03:26 +0530

    Start icon implimentattion

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -8,13 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class CourseComponent implements OnInit {
        title = "This is tile and we want to make it summarize so making a custom pipe";
        email="";
+       startClass="glyphicon glyphicon-star-empty";
        imagePath = "http://lorempixel.com/400/200"
        isActive=true;
+       isStartClicked=true;
        onSave($event){
        console.log("Button was clicked", $event);
        }
        onKeyUp(){
        this.title = this.email;
+       }
+       onSpanClick(){
+                this.isStartClicked = !this.isStartClicked;
+                this.startClass=(this.startClass == "glyphicon glyphicon-star-empty" ? "glyphicon glyphicon-star": "glyphicon glyphicon-star-empty");
+                /**
+                if(this.startClass === "glyphicon glyphicon-star-empty"){
+                        this.startClass = "glyphicon glyphicon-star";                
+                                }
+                else {
+                this.startClass = "glyphicon glyphicon-star-empty";
+                }
+                */
        }
   constructor() { }
 
