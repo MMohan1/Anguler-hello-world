4b8e17641c500f08487fa4e6d0df3a348a9a01ae
MMohan1 <manmohansharma987@gmail.com>
2017-09-14 08:52:27 +0530

    Event biniding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CoursesService } from './courses.service';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './course.component';
@@ -15,7 +16,8 @@ import { AuthorsService } from './authors.service';
     AuthorsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
   CoursesService,
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -7,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseComponent implements OnInit {
        title = "List of Courses";
+       email="";
        imagePath = "http://lorempixel.com/400/200"
        isActive=true;
+       onSave($event){
+       console.log("Button was clicked", $event);
+       }
+       onKeyUp(){
+       this.title = this.email;
+       }
   constructor() { }
 
   ngOnInit() {
